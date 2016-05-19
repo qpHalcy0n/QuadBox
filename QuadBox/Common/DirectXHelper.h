7713f2690e0f4fdaaba7ad34a2dc09041d6214c1
Rciuffo <rciuffo@johnspizza.com>
2016-05-19 13:51:30 -0700

    Added XAML-based GUI for testing.

--- QuadBox/Common/DirectXHelper.h
+++ QuadBox/Common/DirectXHelper.h
@@ -21,10 +21,10 @@ namespace DX
 
 		auto folder = Windows::ApplicationModel::Package::Current->InstalledLocation;
 
-		return create_task(folder->GetFileAsync(Platform::StringReference(filename.c_str()))).then([] (StorageFile^ file) 
+		return create_task(folder->GetFileAsync(Platform::StringReference(filename.c_str()))).then([](StorageFile^ file)
 		{
 			return FileIO::ReadBufferAsync(file);
-		}).then([] (Streams::IBuffer^ fileBuffer) -> std::vector<byte> 
+		}).then([](Streams::IBuffer^ fileBuffer) -> std::vector<byte>
 		{
 			std::vector<byte> returnBuffer;
 			returnBuffer.resize(fileBuffer->Length);
@@ -55,7 +55,7 @@ namespace DX
 			nullptr,                    // No need to keep the D3D device reference.
 			nullptr,                    // No need to know the feature level.
 			nullptr                     // No need to keep the D3D device context reference.
-			);
+		);
 
 		return SUCCEEDED(hr);
 	}
